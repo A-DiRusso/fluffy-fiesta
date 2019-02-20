d2b41ed8cdde7bc8650b69f366bc4b4d267acdb9
Anthony DiRusso <anthonydirusso@Anthonys-MacBook-Pro.local>
2019-02-20 10:16:56 -0500

    updates constuctor to set name attribute

--- character.py
+++ character.py
@@ -3,4 +3,10 @@
 # inventory
 
 class Character():
-    pass
+    #def dunder init :) method is the constructor 
+    # always in python
+    def __init__(self, new_name):
+        # `self` customary way to refers
+        #  to the instance being built
+        # some other languages use `this` 
+        self.name = new_name
